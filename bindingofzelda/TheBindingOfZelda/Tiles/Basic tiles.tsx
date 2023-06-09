f90398b8a7d8c40136d438c96742b849387aefca
robin <robenjay@hotmail.nl>
2023-06-09 14:59:44 +0200

    added a tileset and the maps for the level

---
+++ bindingofzelda/TheBindingOfZelda/Tiles/Basic tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Basic tiles" tilewidth="16" tileheight="16" tilecount="21" columns="7">
+ <image source="../../../../../../Downloads/level1tilemap.png" width="112" height="48"/>
+</tileset>
